c5264e36918ba543ba109b24b1cc46deb0d2faa3
Florian Müller <mueller@tk.tu-darmstadt.de>
2021-09-14 14:24:44 +0200

    ...

--- R/plot.r
+++ R/plot.r
@@ -1,3 +1,81 @@
+#' data_summary_plot_all
+#'
+#' This function produces summary plots of multiple dependent variables given a fixed set of grouping variables
+#' @param data the data
+#' @param varnames vector containing the names of the columns with the dependent variables
+#' @param groupnames a vector of the names of the grouping variables
+#' @param outdir a file.path pointing to the directory to save the plots
+#' @param prefix a string to prefix the file names of the plots
+#' @param p.width (optional) the width of the plot
+#' @param p.height (optional) the height of the plot
+#' @param fun.sum (optional) the summary function to use. Defaults to mean.
+#' @param fun.error (optional) the error function to use. Defaults to sd.
+#' @param p.scale_fill_manual (optional) a color scale for the plot
+#' @export
+data_summary_plot_all <- function(data, varnames, groupnames, outdir, prefix, p.width, p.height, fun.sum = mean, fun.error = sd, scale_fill_manual = NULL) {
+  
+  dir.create(outdir, showWarnings = FALSE)
+  
+  groupnames.sets <- sets::set_power(groupnames)
+  
+  for(set in groupnames.sets) {
+    set <- unlist(set)
+    if(any(length(set) < 1 | length(set) > 4) ) {
+      #message("Sorry, I don't know how to handle the set.")
+    } else {
+      data_summary_plot_multiple(data, varnames, set, outdir, prefix, p.width, p.height, fun.sum, fun.error, scale_fill_manual)
+    }
+  }
+  
+  files <- list.files(outdir, pattern = paste("^", prefix, ".*pdf$", sep=""))
+  cur.wd <- getwd()
+  setwd(outdir)
+  qpdf::pdf_combine(input = files,
+                    output = paste0(prefix, "_summary.pdf"))
+  setwd(cur.wd)
+  
+}
+
+
+#' data_summary_plot_multiple
+#'
+#' This function produces summary plots of multiple dependent variables given a fixed set of grouping variables
+#' @param data the data
+#' @param varnames vector containing the names of the columns with the dependent variables
+#' @param groupnames a vector of the names of the grouping variables
+#' @param outdir a file.path pointing to the directory to save the plots
+#' @param p.width (optional) the width of the plot
+#' @param p.height (optional) the height of the plot
+#' @param prefix (optional) a string to prefix the filenames of the plots
+#' @param fun.sum (optional) the summary function to use. Defaults to mean.
+#' @param fun.error (optional) the error function to use. Defaults to sd.
+#' @param p.scale_fill_manual (optional) a color scale for the plot
+#' @export
+data_summary_plot_multiple <- function(data, varnames, groupnames, outdir, p.width, p.height, prefix = NULL, fun.sum = mean, fun.error = sd, scale_fill_manual = NULL) {
+  
+  groupingString <- paste("_by", paste(groupnames, collapse="_"), sep="_")
+  
+  dir.create(outdir, showWarnings = FALSE)
+  
+  for(varname in varnames) {
+    if(is.null(prefix)) {
+      title <- paste(varname, groupingString, sep="")
+    } else {
+      title <- paste(prefix, "_", varname, groupingString, sep="")
+    }
+
+    p <- data_summary_plot(data, varname, groupnames, fun.sum, fun.error, scale_fill_manual, p.title = title)
+    filename = paste(title, ".pdf", sep="")
+    outPath = file.path(outdir, filename)
+    print(outPath)
+    if(any(missing(p.width) | missing(p.height)))
+      ggsave(outPath, plot=p, device = cairo_pdf)
+    else
+      ggsave(outPath, plot=p, device = cairo_pdf, width = p.width, height = p.height)
+  }
+}
+
+
 #' data_summary_plot
 #'
 #' This function produces a summary plot of your data
